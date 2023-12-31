b01f6ef2ff58a12b45601f1dba0b467378541c1b
dhirendrakumar9032 <dhirendra9032@gmail.com>
2023-12-31 16:52:24 +0530

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+    <div>
+      <div className="text-xl font-medium text-black">Welcome to React with Tailwind!</div>
+      <p className="text-gray-500">You're all set to build something amazing.</p>
+    </div>
+  </div>
+  );
+}
+
+export default App;
